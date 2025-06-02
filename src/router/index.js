756911aa775d91022d05b9b62d2268ab2d80595c
bruno <brunoalcorral@gmail.com>
2025-06-01 21:27:24 -0300

    feat:Adicionar página não encontrada
    - Adicionado o caminho da página não encontrada no arquivo de rotas;
    - Adicionado o arquivo para a página não encontrada;

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import PostView from '../views/PostFavoriteMovie.vue'
+import NotFound from '../views/404View.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -20,6 +21,11 @@ const router = createRouter({
       name: 'post-favorite-movie',
       component: PostView,
     },
+    {
+      path: '/:pathMatch(.*)*',
+      name: 'NotFound',
+      component: NotFound
+    }
   ],
 })
 
---
+++ src/views/404View.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="flex justify-center items-center h-[100%]">
+      <div class="w-[88%]">
+        <h1 class="text-sm sm:text-lg md:text-2xl text-center font-bold mb-2 sm:mb-4">404 - Page not found</h1>
+        <p class="text-sm sm:text-lg md:text-xl text-center mb-2 sm:mb-4">The page you are looking for does not exist.</p>
+        <div class="flex items-center justify-center gap-2 p-3">
+            <router-link to="/">
+                <button class="bg-[#42b983] text-white px-8 md:px-15 py-2 rounded-lg shadow hover:bg-[#52b989] transition cursor-pointer">
+                    <p class="text-sm sm:text-lg md:text-xl">Back to top</p>
+                </button>
+            </router-link>
+        </div>
+      </div>
+    </div>
+</template>
